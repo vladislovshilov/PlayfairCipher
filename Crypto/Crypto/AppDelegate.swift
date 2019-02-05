d078f4b14867bd390f5ca1f4800ec24e6ec52398
Unknown <vladislov.shilov@gmail.com>
2019-02-06 00:56:43 +0300

    Added project

---
+++ Crypto/Crypto/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Crypto
+//
+//  Created by Vlados iOS on 2/5/19.
+//  Copyright © 2019 Vladislav Shilov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
